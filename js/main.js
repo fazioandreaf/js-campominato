ef1cc9fc4ad469aa233879a5b7ac5433693964ca
fazioandrea.f <fazioandrea.f@gmail.com>
2021-03-15 16:43:32 +0100

    perfezionamento

--- js/main.js
+++ js/main.js
@@ -24,16 +24,20 @@ function mineswipper() {
      }
     console.log(numeriPC,numeriPC.length);
     console.log(numeriPCunici,numeriPCunici.length);
-    for (i=16;i<100;i++) {
-        let numeroUtente = parseInt(prompt('Scrivi il tuo numero compreso tra 1 a 99'));
+    var j=0;
+    for (i=17;i<101;i++) {
+        let numeroUtente = parseInt(prompt('Scrivi il tuo ' + i+'-esimo numero compreso tra 1 a 99'));
         if (!(numeroUtente>0 && numeroUtente<100)) {
             return alert ('Non hai inserito un numero compreso tra 1 e 99')
         }
         console.log(numeroUtente, typeof numeroUtente);
         if (!numeriPCunici.includes(numeroUtente)){
               numeriPCunici.push(numeroUtente);
+              j++;
+             
           }else {
-              return alert ('hai inserito giá un numero presente nella lista')
+              return alert ('Hai perso ' + ' Hai inserito ' + j + ' numeri')
+             
           }
           console.log(numeriPCunici,numeriPCunici.length);
     }
