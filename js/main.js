bedc69933a53cc6ffe7c3df194e74e39947c5874
fazioandrea.f <fazioandrea.f@gmail.com>
2021-03-15 16:29:10 +0100

    Esercizio funzionante. Migliorare il codice

--- js/main.js
+++ js/main.js
@@ -15,16 +15,33 @@ function mineswipper() {
     } 
     while(variabile>0){
         for (i=0;i<variabile;i++) {
-            let prova1 = Math.floor(Math.random()*100);
-            if (!numeriPCunici.includes(prova1)){
+            let numeriAggiuntiviPC = Math.floor(Math.random()*100);
+            if (!numeriPCunici.includes(numeriAggiuntiviPC)){
                   numeriPCunici.push(numeriPC[numeriPCunici.length]);
               }
         }
         variabile= numeriPC.length - numeriPCunici.length ;
      }
-        
     console.log(numeriPC,numeriPC.length);
     console.log(numeriPCunici,numeriPCunici.length);
+    for (i=16;i<100;i++) {
+        let numeroUtente = parseInt(prompt('Scrivi il tuo numero compreso tra 1 a 99'));
+        if (!(numeroUtente>0 && numeroUtente<100)) {
+            return alert ('Non hai inserito un numero compreso tra 1 e 99')
+        }
+        console.log(numeroUtente, typeof numeroUtente);
+        if (!numeriPCunici.includes(numeroUtente)){
+              numeriPCunici.push(numeroUtente);
+          }else {
+              return alert ('hai inserito giá un numero presente nella lista')
+          }
+          console.log(numeriPCunici,numeriPCunici.length);
+    }
+    return alert('hai vinto!')
+   
+ 
+
+
         
 }
     
