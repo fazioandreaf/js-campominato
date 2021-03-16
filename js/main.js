9a561009b390c5784cffef04a727b8663a32cd65
andrea <fazioadndrea>
2021-03-16 14:41:59 +0100

    Uso della funzione controllArr

--- js/main.js
+++ js/main.js
@@ -1,27 +1,24 @@
-function mineswipper(dimMax, numerinoti) {
-    var numeriPC= [];
-    var numeriPCunici= [];
-    for (i=0;i<numerinoti;i++) {
-        let value= Math.floor(Math.random()*(dimMax+1));
-        if (!numeriPCunici.includes(value)){
-              numeriPCunici.push(value);
+function controlloArr (maxLenghtArr,dimMax,arr) {
+    for (i=0;i<maxLenghtArr;i++) {
+        let value= Math.floor(Math.random()*(dimMax)+1);
+        if (!arr.includes(value)){
+              arr.push(value);
           }
     }
+}
+function mineswipper(dimMax, numerinoti) {
+    var numeriPCunici= [];
+    controlloArr (numerinoti,dimMax,numeriPCunici);
     var variabile=0;
     if (numeriPCunici.length<numerinoti) {
         variabile= numerinoti - numeriPCunici.length 
     } 
     while(variabile>0){
-        for (i=0;i<variabile;i++) {
-            let numeriAggiuntiviPC = Math.floor(Math.random()*(dimMax+1));
-            if (!numeriPCunici.includes(numeriAggiuntiviPC)){
-                  numeriPCunici.push(numeriAggiuntiviPC);
-              }
-        }
+        controlloArr (variabile,dimMax,numeriPCunici);
         variabile= numerinoti - numeriPCunici.length ;
      }
     console.log(numeriPCunici,numeriPCunici.length);
-    document.getElementById('numeriPChtml').innerHTML += numeriPCunici;
+    document.getElementById('numeriPChtml').innerHTML= numeriPCunici;
     var j=0;
     for (i=numerinoti;i<dimMax;i++) {
         let numeroUtente = parseInt(prompt('Scrivi il tuo ' + i+'-esimo numero compreso tra 1 a ' + dimMax));
@@ -31,10 +28,8 @@ function mineswipper(dimMax, numerinoti) {
         if (!numeriPCunici.includes(numeroUtente)){
               numeriPCunici.push(numeroUtente);
               j++;
-             
           }else {
               return alert ('Hai perso ' + ' Hai inserito ' + j + ' numeri')
-             
           }
           console.log(numeriPCunici,numeriPCunici.length);
     }
