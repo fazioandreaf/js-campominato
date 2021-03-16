2782c962f1e00301a156f332059513cec98eee22
andrea <fazioadndrea>
2021-03-16 15:03:06 +0100

    Switch per il bonus

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//funzione per controllare i valori dell array se sono unici
 function controlloArr (maxLenghtArr,dimMax,arr) {
     for (i=0;i<maxLenghtArr;i++) {
         let value= Math.floor(Math.random()*(dimMax)+1);
@@ -9,6 +10,7 @@ function controlloArr (maxLenghtArr,dimMax,arr) {
 function mineswipper(dimMax, numerinoti) {
     var numeriPCunici= [];
     controlloArr (numerinoti,dimMax,numeriPCunici);
+    //controllare se l'array costruito sia della dimensione scelta dallo switch o differisce
     var variabile=0;
     if (numeriPCunici.length<numerinoti) {
         variabile= numerinoti - numeriPCunici.length 
@@ -17,8 +19,11 @@ function mineswipper(dimMax, numerinoti) {
         controlloArr (variabile,dimMax,numeriPCunici);
         variabile= numerinoti - numeriPCunici.length ;
      }
+     //debug per capire se tutto fila lisco
     console.log(numeriPCunici,numeriPCunici.length);
+    //mostrare a video i numeri del pc
     document.getElementById('numeriPChtml').innerHTML= numeriPCunici;
+    //variabile per poi comunicare all' utente quanti numeri ha messo prima di perdere
     var j=0;
     for (i=numerinoti;i<dimMax;i++) {
         let numeroUtente = parseInt(prompt('Scrivi il tuo ' + i+'-esimo numero compreso tra 1 a ' + dimMax));
@@ -35,8 +40,26 @@ function mineswipper(dimMax, numerinoti) {
     }
     return alert('hai vinto!')
 }
-var difficolta =  parseInt(prompt('Inserisci il numero della griglia'));
-var n_numeriPC =  parseInt(prompt('Inserisci i numeri giá dati dal pc'));
+//var difficolta =  parseInt(prompt('Inserisci il numero della griglia'));
+//var n_numeriPC =  parseInt(prompt('Inserisci i numeri giá dati dal pc'));
+//con difficoltà 0 => tra 1 e 100
+//con difficoltà 1 => tra 1 e 80
+//con difficoltà 2 => tra 1 e 50
+var difficolta= parseInt(prompt('Vuoi giocare con che difficoltà? 0 minimo, 1 o 2 massimo '));
+switch(difficolta){
+    case 0:
+        difficolta=100
+        break;
+    case 1:
+        difficolta= 80
+        break;
+    case 2:
+        difficolta= 50
+        break;
+    default: prompt('Non hai inserito un numero fra quelli proposti')
+}
+var n_numeriPC =16;
+
 
 
     
